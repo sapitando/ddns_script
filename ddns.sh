e2f6019c90bcaddc005e9d58e66106deea0353a4
sapitando <111522180+sapitando@users.noreply.github.com>
2022-09-19 13:12:18 -0300

    #commit

--- ddns.sh
+++ ddns.sh
@@ -11,15 +11,6 @@ DATA_FILE=~/.ddns/data
 LOG_FILE=~/.ddns/log
 
 # Generate "data" and "log" files if not found
-if [ ! -f "$LOG_FILE" ]; then
-  DIR_LOG_FILE=$(printf "%s" "$LOG_FILE" | sed -E '/\/$/d; s/((.*)\/|)[[:alnum:]]+[^/]$/\2/')
-  [ -n "$DIR_LOG_FILE" ] && [ ! -d "$DIR_LOG_FILE" ] && mkdir -p "$DIR_LOG_FILE"
-  printf "\033[30;46m>DDNS script...\033[m\n" >"$LOG_FILE"
-  if [ ! -f "$LOG_FILE" ]; then
-    printf "\033[30;101mLog file not created...\033[m\n"
-    exit 1
-  fi
-fi
 if [ ! -f "$DATA_FILE" ]; then
   DIR_DATA_FILE=$(printf "%s" "$DATA_FILE" | sed -E '/\/$/d; s/((.*)\/|)[[:alnum:]]+[^/]$/\2/')
   [ -n "$DIR_DATA_FILE" ] && [ ! -d "$DIR_DATA_FILE" ] && mkdir -p "$DIR_DATA_FILE"
@@ -32,6 +23,15 @@ else
   # Get current ip for comparison and check if there were failed updades
   LAST_IPV6=$(grep -Pom 1 '(?<=^ipv6=).*' "$DATA_FILE")
   NOT_UPDATED=$(grep -Pom 1 '(?<=^not_updated=).*' "$DATA_FILE")
+fi
+if [ ! -f "$LOG_FILE" ]; then
+  DIR_LOG_FILE=$(printf "%s" "$LOG_FILE" | sed -E '/\/$/d; s/((.*)\/|)[[:alnum:]]+[^/]$/\2/')
+  [ -n "$DIR_LOG_FILE" ] && [ ! -d "$DIR_LOG_FILE" ] && mkdir -p "$DIR_LOG_FILE"
+  printf "\033[30;46m>DDNS script...\033[m\n" >"$LOG_FILE"
+  if [ ! -f "$LOG_FILE" ]; then
+    printf "\033[30;101mLog file not created...\033[m\n"
+    exit 1
+  fi
 fi
 
 # Get current ipv6 if available
